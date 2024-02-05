75a1e15d48d02d44281feb2f6e7ffd9013115729
James Udani <udani.jamescharlie@gmail.com>
2024-02-04 19:10:43 -0500

    fixing ui

--- app/(components)/CityList.tsx
+++ app/(components)/CityList.tsx
@@ -6,14 +6,21 @@ interface CityListProps {
 
 const CityList: React.FC<CityListProps> = ({ cities, onCitySelect }) => {
   return (
-    <div>
-      {cities.map((city) => (
-        <button key={city} onClick={() => onCitySelect(city)}>
-          <h4 className="p-3 text-start backdrop-blur-sm bg-white/20 text-white m-1 w-60 rounded-xl">
-            {city}
-          </h4>
-        </button>
-      ))}
+    <div className="p-2">
+      <h4 className="">Favorites</h4>
+      <ul className="pt-3 flex flex-row  lg:flex-col overflow-x-auto text-white">
+        {cities.map((city) => (
+          <li>
+            <button
+              className="p-3 me-3 lg:mb-3 text-start p-backdrop-blur-sm bg-white/20 w-60 rounded-xl"
+              key={city}
+              onClick={() => onCitySelect(city)}
+            >
+              {city}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -78,35 +78,39 @@ const Home: React.FC = () => {
   }, [city]);
   return (
     <main
-      className="min-h-screen top-0 left-0 right-0 bottom-0 bg-[url('https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-cover bg-fixed
+      className="text-white min-h-screen top-0 left-0 right-0 bottom-0 bg-[url('https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-cover bg-fixed
     p-10
     "
     >
-      <div className="backdrop-blur-sm bg-white/20 rounded-3xl p-5 ">
-        <div className=" rounded-xl p-2">
-          <form className="flex justify-between w-full">
-            <input
-              onChange={(e) => setSearch(e.target.value)}
-              type="text"
-              placeholder="Search city"
-              className="bg-transparent placeholder:text-gray-300 text-white/90  w-full focus:outline-none text-2xl"
-            />
-            <button className="p-2 text-white/90" onClick={handleSubmit}>
-              <BsSearch size={20} />
-            </button>
-          </form>
-        </div>
-      </div>
-      <CityList cities={cityList} onCitySelect={handleCitySelection} />
-      <div className=" mt-10 ">
-        <div className="backdrop-blur-xl bg-white/10  rounded-2xl p-10 ">
-          {weather ? (
-            <Weather weather={weather} />
-          ) : (
-            <h5 className="text-center m-auto text-white p-10">
-              City not found
-            </h5>
-          )}
+      <h2>Weadapp</h2>
+      <div className="flex flex-col lg:flex-row ">
+        <CityList cities={cityList} onCitySelect={handleCitySelection} />
+
+        <div>
+          <div className="backdrop-blur-sm bg-white/20 rounded-3xl p-5 ">
+            <div className=" rounded-xl p-2">
+              <form className="flex justify-between w-full">
+                <input
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="text"
+                  placeholder="Search city"
+                  className="bg-transparent placeholder:text-gray-300 text-white/90  w-full focus:outline-none text-2xl"
+                />
+                <button className="p-2 text-white/90" onClick={handleSubmit}>
+                  <BsSearch size={20} />
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="  backdrop-blur-xl bg-white/10  rounded-2xl p-10 ">
+            {weather ? (
+              <Weather weather={weather} />
+            ) : (
+              <h5 className="text-center m-auto text-white p-10">
+                City not found
+              </h5>
+            )}
+          </div>
         </div>
       </div>
     </main>
